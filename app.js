ab981a399265a246bceb676885cce7e0c9965639
malikaam <68324481+malikaam@users.noreply.github.com>
2020-10-20 17:44:16 +1100

    card actions and hand

--- app.js
+++ app.js
@@ -22,3 +22,29 @@ const PORT = 3000;
 app.listen(PORT, () => {
     console.log("Server listening on http://localhost:" + PORT + "/game");
 });
+
+
+//Card deck
+
+const express = require('express');
+const app = express();
+const server = require('http').Server(app);
+const escape = require('escape-html');
+
+
+app.use(express.static('public'));
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/public/table.html');
+});
+
+
+
+
+
+
+
+
+
+server.listen(1433, function() {
+    console.log('Server Started On 127.0.0.1:1433');
+});
