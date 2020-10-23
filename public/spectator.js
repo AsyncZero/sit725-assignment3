a6b37040ebef9c95a1ad5cf2d58233950a8d5725
Nicolai <nmahinay@gmail.com>
2020-10-23 09:28:45 +0800

    added send message function for spectator

--- public/spectator.js
+++ public/spectator.js
@@ -1,6 +1,46 @@
 let userID;
+let username;
 let spectators = [];
+let room = "spectators";
 
+<<<<<<< HEAD
 function addSpectator(userID) {
   spectators.push(userID);
 }
+=======
+//add to spectator list
+function addSpectator(userID, username) {
+    let data = {
+        userID: userID,
+        username: username,
+        room: room
+    }
+    spectators.push(data)
+}
+
+//remove player buttons
+
+//hide spectator chats from players
+
+function sendMSG(message, userID) {
+    let spectator;
+    spectators.forEach(function(s) {
+        if(spectators.userID == userID){
+            spectator = s;
+        }
+    })
+    let data ={
+        spectator: spectator,
+        message: message
+    }
+    return data;
+}
+    
+//set card view of spectators
+
+//greet spectator after successfully joining
+
+//spectator left log
+
+//let spectator know if game has updated
+>>>>>>> added send message function for spectator
