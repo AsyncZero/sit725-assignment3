99a364a8d79817854b654f30dc77ce2a88a7d9dc
amanda.haydaw <amanda.haydaw@accenture.com>
2020-10-23 21:41:43 +1100

    add files

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { openConnectionPlayers } = require("./services/MongoService");
 const mongo = require("./services/MongoService");
 let app = new express();
 
@@ -6,19 +7,30 @@ let app = new express();
 mongo.openConnectionPlayers();
 
 //printing logs
-var log = function (logMessage) {
-  var time = moment().format();
-  console.log("[" + time + "]" + "" + logMessage);
+var log = function(logMessage) {
+    var time = moment().format();
+    console.log("[" + time + "]" + "" + logMessage);
 };
 //direct app to fetch relevant files
 app.use(express.static(__dirname + "/public"));
 //set endpoint to html file
-app.get("/Game", function (req, res) {
-  res.sendFile(__dirname + "/public/HomePage.html");
+app.get("/Game", function(req, res) {
+    res.sendFile(__dirname + "/public/HomePage.html");
 });
 
-//Server Run
+
+
+app.get("/player", function(req, res) {
+    let players = req.query.players;
+    mongo.insertPlayer(players);
+    res.send('player added')
+})
+
+app.get('/playerLists', function(req, res) {
+        mongo.retrieveMessages(res)
+    })
+    //Server Run
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log("Server listening on http://localhost:" + PORT + "/game");
+    console.log("Server listening on http://localhost:" + PORT + "/game");
 });
